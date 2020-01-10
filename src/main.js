1523e0333920eff6c84b8c9ff860eb6ea4454f20
runningsparrow <runningsparrow@gmail.com>
2020-01-10 16:56:45 +0800

    add babel-plugin-import

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from "vue";
+// import Antd from 'ant-design-vue';
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// import "ant-design-vue/dist/antd.less";
+import { Button } from "ant-design-vue";
+// import "ant-design-vue/lib/button/style";
 
 Vue.config.productionTip = false;
+
+// Vue.use(Antd)
+Vue.use(Button);
 
 new Vue({
   router,
