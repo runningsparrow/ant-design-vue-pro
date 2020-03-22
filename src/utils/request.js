1a259b5b006addbcb06a0ff9efb619b32e69454f
runningsparrow <runningsparrow@gmail.com>
2020-03-22 14:53:20 +0800

    request

---
+++ src/utils/request.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { notification } from "ant-design-vue";
+
+function request(options) {
+  return axios(options)
+    .then(res => {
+      return res;
+    })
+    .catch(error => {
+      const {
+        response: { status, statusText }
+      } = error;
+      notification.error({
+        // message: status,
+        //加一个 eslint的注释用于对未使用的变量报错
+        // eslint-disable-next-line no-unused-vars
+        message: h => (
+          <div>
+            请求错误 <span style="color: red">{status}</span>: {options.url}
+          </div>
+        ),
+        description: statusText
+      });
+      return Promise.reject(error);
+    });
+}
+
+export default request;
