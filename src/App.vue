f2c837eb66b75419524c0089f4e85c98216a273c
runningsparrow <runningsparrow@gmail.com>
2020-03-30 16:47:53 +0800

    locale

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <template>
   <div id="app">
-    <!-- <a-button>按钮</a-button> -->
-    <div id="nav">
+    <a-locale-provider :locale="locale">
+      <!-- <a-button>按钮</a-button> -->
+      <!-- <div id="nav"> -->
       <!-- <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> -->
       <!-- <router-link to="/dashboard/analysis">dashboard</router-link> |
       <router-link to="/form">form</router-link> -->
-    </div>
-    <router-view />
+      <!-- </div> -->
+      <router-view />
+    </a-locale-provider>
   </div>
 </template>
 
+<script>
+import zhCN from "ant-design-vue/lib/locale-provider/zh_CN";
+import enUS from "ant-design-vue/lib/locale-provider/en_US";
+//引入moment
+import moment from "moment";
+export default {
+  data() {
+    return {
+      locale: zhCN
+    };
+  },
+  watch: {
+    "$route.query.locale": function(val) {
+      this.locale = val === "enUS" ? enUS : zhCN;
+      moment.locale(val === "enUS" ? "en" : "zh-cn");
+    }
+  }
+};
+</script>
 <style lang="less"></style>
--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="header">
-    Header
+    <a-dropdown>
+      <a-icon type="global" />
+      <a-menu
+        slot="overlay"
+        @click="localeChange"
+        :selectdKeys="[$route.query.locale || 'zhCN']"
+      >
+        <a-menu-item key="zhCN">
+          中文
+        </a-menu-item>
+        <a-menu-item key="enUS">
+          English
+        </a-menu-item>
+      </a-menu>
+    </a-dropdown>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    localeChange({ key }) {
+      this.$router.push({ query: { ...this.$router.query, locale: key } });
+    }
+  }
+};
 </script>
 
 <style>
 .header {
   float: right;
+  margin-right: 30px;
 }
 </style>
